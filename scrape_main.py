204a8b44ddc64bd2833e8cf1a6d179ba6758f80f
Justin McDonald <niceguy3088@gmail.com>
2024-06-28 16:50:45 -0700

    Cleaned up variable and function names

--- scrape_main.py
+++ scrape_main.py
@@ -14,15 +14,15 @@ import gc
 
 
 
-def create_database(db_name: str):
-    # Connect to the database (it will be created if it doesn't exist)
-    conn = sqlite3.connect(db_name)
-    print(f"Connected to database {db_name}")
+def createDatabase(dbName: str):
+    # Connect/create database
+    conn = sqlite3.connect(dbName)
+    print(f"Connected to database {dbName}")
 
     # Create a cursor object
     cursor = conn.cursor()
 
-    # Create a table named 'users's
+    # Create a table
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS web (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,23 +32,23 @@ def create_database(db_name: str):
     ''')
     print("Table 'web' created")
 
-    # Commit the changes and close the connection.d
+    # Commit and connection
     conn.commit()
     conn.close()
-    print(f"Database {db_name} closed")
+    print(f"Database {dbName} closed")
 
 # Add 1st URL to DB
-def add_starting_url(starting_url: str, db_name: str):
-    conn = sqlite3.connect(db_name)
+def startURL(startingURL: str, dbName: str):
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
-    cursor.execute("""INSERT INTO web (url, scraped) VALUES (?, ?)""", (starting_url, 0))
+    cursor.execute("""INSERT INTO web (url, scraped) VALUES (?, ?)""", (startingURL, 0))
     conn.commit()
     conn.close()
 
-def web_scraper(url: str) -> list:
-    url_bool = True
-    count_issue = 0
-    while url_bool:
+def webScrape(url: str) -> list:
+    scrapeLoop = True
+    failCount = 0
+    while scrapeLoop:
         try:
             url_list = []
 
@@ -91,80 +91,80 @@ def web_scraper(url: str) -> list:
             driver.quit()
             print(f'Grab URL Count: {str(grab_count)}')
             print(f'Grab Non-duplicate URL count: {str(len(url_list))}')
-            url_bool = False
+            scrapeLoop = False
         except Exception as e:
-            f = open("webscrape_error.log", "a")
+            f = open("error.log", "a")
             f.write(f'\ndatetime.now(): {datetime.now()}\n')
             f.write(f'Error URL: {url}\n')
             f.write(str(e))
             f.close()
             driver.quit()
 
-            count_issue = count_issue + 1
-            if count_issue == 2:
-                url_bool = False
+            failCount = failCount + 1
+            if failCount == 2:
+                scrapeLoop = False
             print(f"Driver (get) error: {str(e)}")
             time.sleep(5)
     return(url_list)
 
 
-# Grab only URLs that have not yet been scrapped
-def target_url_from_db(db_name: str) -> list:
-    db_list_removelist_removetuple = []
-    conn = sqlite3.connect(db_name)
+# Grab URLs that have not been scrapped
+def grabTargetURLs(dbName: str) -> list:
+    removeTuple = []
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
     try:
         cursor.execute("SELECT url, scraped FROM web WHERE scraped = ?", (0,))
         rows = cursor.fetchall()
         for row in rows:
-            db_list_removelist_removetuple.append(row[0])
+            removeTuple.append(row[0])
     except:
         conn.close()
-        print("Error on target_url_from_db function")
+        print("Error on grabTargetURLs function")
     conn.close()
-    return (db_list_removelist_removetuple)
+    return (removeTuple)
 
 # Randomly select URL from target list
-def random_select_list(pre_url_list: list) -> str:
+def grabrandomURL(pre_url_list: list) -> str:
     int_rand = random.randrange(0, len(pre_url_list))
     url_final = pre_url_list[int_rand]
     return(url_final)
 
-# Load URLs from DB to compare to web scrape list
-def load_db(db_name: str) -> list:
-    db_list_removelist_removetuple = []
-    conn = sqlite3.connect(db_name)
+# Load all URLs from DB
+def loadDBList(dbName: str) -> list:
+    removeTuple = []
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
     cursor.execute("SELECT url, scraped FROM web")
     rows = cursor.fetchall()
     for row in rows:
-        db_list_removelist_removetuple.append(row[0])
+        removeTuple.append(row[0])
     conn.close()
 
-    print(f'DB URL Count: {str(len(db_list_removelist_removetuple))}')
-    return(db_list_removelist_removetuple)
+    print(f'DB URL Count: {str(len(removeTuple))}')
+    return(removeTuple)
 
 # Compare non-duplicate list to DB list
-def compare_db(url_list: list, current_db_list: list) -> list:
-    temp_list = []
+def compareList(url_list: list, current_db_list: list) -> list:
+    tempList = []
     for url_item in url_list:
         if url_item not in current_db_list:
-            temp_list.append([url_item, 0])
+            tempList.append([url_item, 0])
 
-    return(temp_list)
+    return(tempList)
 
-# Parse Scraped URLs to Domain level
-def parseDomains(final_list: list) -> list:
+# Parse scraped URLs to domain level
+def parseDomains(vettedList: list) -> list:
     parsedDomains = []
     print('Parsing Domains...')
-    for url_item in final_list:
+    for url_item in vettedList:
         parsed_uri = urlparse(url_item[0])
         domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
         parsedDomains.append(domain)
     
     return (parsedDomains)
 
-# Count Parsed URL Domains. Add Domains & Count to list
+# Count parsed URL domains and add domains & count to list
 def countOccurrences(parseDomainsList: list) -> list:
     nonDuplicate = []
     domainListCount = []
@@ -181,9 +181,9 @@ def countOccurrences(parseDomainsList: list) -> list:
             print(f'URL: {url_item} Count: {count}')
     return (domainListCount)
 
-# Update Domain Counts in DB
-def updateDomainCount(DomainCountList: list, db_name: str):
-    conn = sqlite3.connect(db_name)
+# Update or add new domain counts in DB
+def updateDomainCount(DomainCountList: list, dbName: str):
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
     for item in DomainCountList:
         cursor.execute("""INSERT INTO domains (SLDomain, DCount) VALUES (?, ?)
@@ -191,46 +191,48 @@ def updateDomainCount(DomainCountList: list, db_name: str):
         conn.commit()
     conn.close()
 
-# Load Domains and Counts to list
-def loadDomains(db_name: str) -> list:
-    db_list_removelist_removetuple = []
-    conn = sqlite3.connect(db_name)
+# Load domains and counts to list
+def loadDomains(dbName: str) -> list:
+    removeTuple = []
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
     cursor.execute("SELECT SLDomain, DCount FROM domains")
     rows = cursor.fetchall()
     conn.close()
+
     for row in rows:
-        db_list_removelist_removetuple.append([row[0], row[1]])
+        removeTuple.append([row[0], row[1]])
 
+    print(f'DB Domain Count: {str(len(removeTuple))}')
+    return(removeTuple)
 
-    print(f'DB Domain Count: {str(len(db_list_removelist_removetuple))}')
-    return(db_list_removelist_removetuple)
-
-# Write URLs to DB and update
-def write_completed_db(final_list, db_name, url, loadDomainsList, maxDomainHit):
-    write_finallist = []
-    in_domain_list = False
-    for fitem in final_list:
-        in_domain_list = False
+# Write URLs to DB
+# If URL is > maxDomainHit, save as 1
+# If URL is < maxDomainHit, save as 0
+def updateListDB(vettedList, dbName, url, domainsList, maxDomainHit):
+    finalList = []
+    foundDomain = False
+    for fitem in vettedList:
+        foundDomain = False
         parsed_uri = urlparse(fitem[0])
         fitemdomain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-        for ditem in loadDomainsList:
+        for ditem in domainsList:
             if fitemdomain == ditem[0] and ditem[1] > maxDomainHit:
-                write_finallist.append([fitem[0], 1])
-                in_domain_list = True
+                finalList.append([fitem[0], 1])
+                foundDomain = True
                 break
             elif fitemdomain == ditem[0] and ditem[1] < maxDomainHit:
-                write_finallist.append([fitem[0], 0])
-                in_domain_list = True
+                finalList.append([fitem[0], 0])
+                foundDomain = True
                 break
             else:
-                in_domain_list = False
-        if in_domain_list == False:
-            write_finallist.append([fitem[0], 0])
+                foundDomain = False
+        if foundDomain == False:
+            finalList.append([fitem[0], 0])
 
-    conn = sqlite3.connect(db_name)
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
-    for item in write_finallist:
+    for item in finalList:
         cursor.execute("""INSERT OR IGNORE INTO web (url, scraped) VALUES (?, ?)""", (item[0], item[1]))
         conn.commit()
     try:
@@ -241,9 +243,9 @@ def write_completed_db(final_list, db_name, url, loadDomainsList, maxDomainHit):
     conn.close()
 
 if __name__ == "__main__":
-    db_name = 'webscraper.db'
-    int_low_wait_range = 1000 # 1000 = 1s
-    int_high_wait_range = 2000 # 1000 = 1s
+    dbName = 'webscraper.db'
+    minWait = 1000 # 1000 = 1s
+    maxWait = 2000 # 1000 = 1s
     maxDomainHit = 100
     py_argu = sys.argv
    
@@ -251,44 +253,44 @@ if __name__ == "__main__":
     # run .py with 2 argument to generate DB & add starting URL
     if len(py_argu) > 1:
         if len(py_argu) == 2:
-            create_database(db_name)
+            createDatabase(dbName)
         elif len(py_argu) == 3:
-            starting_url = str(py_argu[2])
-            create_database(db_name)
-            add_starting_url(starting_url, db_name)
+            startingURL = str(py_argu[2])
+            createDatabase(dbName)
+            startURL(startingURL, dbName)
  
     while True:
-        pre_url_list = target_url_from_db(db_name)
+        pre_url_list = grabTargetURLs(dbName)
         if len(pre_url_list) == 0:
             print('Scrapping complete or error.')
             quit()
-        url = random_select_list(pre_url_list)
+        url = grabrandomURL(pre_url_list)
         
         print('Target URL: ' + str(url))
 
-        web_url_list = web_scraper(url)
+        scrapeList = webScrape(url)
         
-        # gc improved from 4 seconds to .5 seconds for load_db function
+        # gc improved from 4 seconds to .5 seconds for loadDBList function
         gc.collect()
 
-        current_db_list = load_db(db_name)
-        final_list = compare_db(web_url_list, current_db_list)
+        current_db_list = loadDBList(dbName)
+        vettedList = compareList(scrapeList, current_db_list)
 
         # Parse URLs for Domains.
         # Add up total Domains and track
         # Update DB for Domains and Counts
         # Reload Domain & Count List
-        parseDomainsList = parseDomains(final_list)
+        parseDomainsList = parseDomains(vettedList)
         DomainCountList = countOccurrences(parseDomainsList)
-        updateDomainCount(DomainCountList, db_name)
-        loadDomainsList = loadDomains(db_name)
+        updateDomainCount(DomainCountList, dbName)
+        domainsList = loadDomains(dbName)
 
         # Use Domain Count Lists to write New URLS to DB
         # So that Domains are not hit more then max hits
-        write_completed_db(final_list, db_name, url, loadDomainsList, maxDomainHit)
+        updateListDB(vettedList, dbName, url, domainsList, maxDomainHit)
 
-        float_rand_sleep_delay = float((random.randrange(int_low_wait_range, int_high_wait_range)/1000))        
-        print(f'Sleeping for {str(float_rand_sleep_delay)}\n')
+        randSleep = float((random.randrange(minWait, maxWait)/1000))        
+        print(f'Sleeping for {str(randSleep)}\n')
         gc.collect()        
-        time.sleep(float_rand_sleep_delay)
+        time.sleep(randSleep)
 
